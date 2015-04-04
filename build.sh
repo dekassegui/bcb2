250c5153d1582ec3e1169c0cf007af7c3d18bcb8
dekassegui <dekassegui@gmail.com>
2015-04-04 05:41:36 -0300

    Melhorias no script de montagem do projeto.

--- build.sh
+++ build.sh
@@ -2,27 +2,21 @@
 #
 # Script p/montagem do projeto, alternativo ao uso do ANT + build file.
 #
-# criação e preenchimento do diretório das dependências (libraries)
-source mkLinks.sh
-# criação do classpath de compilação
-declare -a jarFiles
-for f in lib/*.jar; do
-  jarFiles=( ${jarFiles[*]} $f )
-done
-jarFiles=$( echo ${jarFiles[*]} | tr ' ' ':' )
-# criação da lista de arquivos fonte
-declare -a sourceFiles
-for f in BCBCliente ClienteBCB SafeDateFormat; do
-  sourceFiles=( ${sourceFiles[*]} "sources/neo/${f}.java" )
-done
 # criação ou reinicio do diretório destino da compilação
 [[ -d build ]] && rm -fr build/* || mkdir build
+# criação e preenchimento do diretório das dependências (libraries)
+source mkLinks.sh
+declare -a dependencias=( lib/*.jar )
 # compilação
-javac -cp $jarFiles -d build/ -Xlint -Werror ${sourceFiles[*]}
+PREV_IFS="$IFS"
+IFS=':'
+javac -cp "${dependencias[*]}" -d build/ -Xlint -Werror sources/neo/*.java
+IFS="$PREV_IFS"
 # criação de arquivo MANIFEST "a priori"
-cat > /tmp/manifest.txt <<DOC
+MANIFEST='/tmp/manifest.txt'
+cat > $MANIFEST <<DOC
 Main-Class: neo.ClienteBCB
-Class-Path: $( echo $jarFiles | sed 'y/:/ /; s|lib/||g' )
+Class-Path: ${dependencias[*]//lib\//}
 DOC
-# criação do principal jarfile desse projeto
-jar cvfm "lib/bcb.jar" /tmp/manifest.txt resources/ cfg/ -C build/ .
+# criação do jarfile desse projeto
+jar cvfm "lib/bcb.jar" $MANIFEST resources/ cfg/ -C build/ . > /dev/null 2>&1
